b27a3778df55cf921ba139fcf35db007496347c3
REN_22 <renzo.bastide@etu.iut-tlse3.fr>
2023-03-14 09:52:41 +0100

    api google comencé Cannot resolve symbol 'Builder' Cannot resolve method 'events' in 'Calendar'

--- app/src/main/java/com/example/isync/gogle.java
+++ app/src/main/java/com/example/isync/gogle.java
@@ -3,6 +3,9 @@ package com.example.isync;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Event;
 
 import net.fortuna.ical4j.data.CalendarBuilder;
 import net.fortuna.ical4j.data.ParserException;
@@ -12,33 +15,39 @@ import net.fortuna.ical4j.model.component.VEvent;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class gogle {
-    public static void supprr(int num) throws IOException, ParserException {
-        class CredentialHelper {
-            public static GoogleCredentials getCredentials() throws IOException {
-                FileInputStream credentialsStream = new FileInputStream("C:/Users/renzo/Documents/ISYNC/certifica.json");
-                GoogleCredentials credentials = GoogleCredentials.fromStream(credentialsStream);
-                return credentials;
-            }
-        }
+public class GoogleCalendarUtils {
+
+    private static final String APPLICATION_NAME = "iSync";
+    private static final JacksonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
+    /**
+     * Supprime tous les événements d'un fichier ICS de Google Calendar.
+     *
+     * @param num le numéro du fichier ICS à synchroniser
+     * @throws IOException
+     * @throws ParserException
+     * @throws GeneralSecurityException
+     */
+    public static void deleteAllEvents(int num) throws IOException, ParserException, GeneralSecurityException {
         // Créer un objet File pour le fichier ICS
         File file = new File("PASTOUCHE/g" + num + "N.ics");
         CalendarBuilder builder = new CalendarBuilder();
-        Calendar calendar = builder.build(new FileInputStream(file));
-        List<VEvent> events = ((Calendar) calendar).getComponents().stream()
+        net.fortuna.ical4j.model.Calendar calendar = builder.build(new FileInputStream(file));
+        List<VEvent> events = calendar.getComponents().stream()
                 .filter(component -> component instanceof VEvent)
                 .map(component -> (VEvent) component)
                 .collect(Collectors.toList());
 
         // Supprimer les évènements de Google Calendar
-        GoogleCredentials credentials = CredentialHelper.getCredentials();
-        NetHttpTransport httpTransport = new NetHttpTransport();
-        JacksonFactory jsonFactory = new JacksonFactory();
-        Calendar service = new Calendar.Builder(httpTransport, jsonFactory, credentials)
-                .setApplicationName("iSync")
+        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream("C:/Users/renzo/Documents/ISYNC/certifica.json"))
+                .createScoped(Collections.singleton(CalendarScopes.CALENDAR));
+        Calendar service = new Calendar.Builder(new NetHttpTransport(), JSON_FACTORY, credentials)
+                .setApplicationName(APPLICATION_NAME)
                 .build();
         String calendarId = "primary";
         for (VEvent event : events) {
